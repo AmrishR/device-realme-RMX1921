15e95209e44e2f038b8e46ed58f54d09d8272066
Harshit Jain <god@harsh1998.me>
2020-07-02 03:21:29 +0530

    RMX1921: fod: implement in-hal handling of doze state
    * Fixes fod on AOD
    * Remove useless HBM node, With fod extentions its now handeled by kernel
    * depends on commit dev-harsh1998/android_kernel_realme_sm6150@b4160f6a346457e2f003222ea6d0634fce71f9bf
    
    Signed-off-by: Harshit Jain <god@harsh1998.me>
    Signed-off-by: AmrishR <ramrish7799@gmail.com>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -28,6 +28,9 @@
 #define DIMLAYER_PATH "/sys/kernel/oppo_display/dimlayer_hbm"
 #define X_POS 445
 #define Y_POS 1967
+#define NOTIFY_BLANK_PATH "/sys/kernel/oppo_display/notify_panel_blank"
+#define AOD_MODE_PATH "/sys/kernel/oppo_display/aod_light_mode_set"
+#define DOZE_STATUS "/proc/touchpanel/DOZE_STATUS"
 #define FP_SIZE 196
 #define FP_BEGIN 1
 #define FP_ENDIT 0
@@ -103,6 +106,10 @@ Return<void> FingerprintInscreen::onRelease() {
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
+    if(get(DOZE_STATUS, FP_ENDIT)) {
+    set(NOTIFY_BLANK_PATH, FP_BEGIN);
+    set(AOD_MODE_PATH, FP_BEGIN);
+    }
     return Void();
 }
 
