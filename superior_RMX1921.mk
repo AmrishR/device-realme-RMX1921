765f221eb44ac4f1da2e8691bcbc33b2d22fadc3
Amrish Ramesh <44675534+AmrishR@users.noreply.github.com>
2020-07-21 10:32:52 +0530

    Update superior_RMX1921.mk

--- superior_RMX1921.mk
+++ superior_RMX1921.mk
@@ -31,8 +31,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1921" \
     TARGET_DEVICE="RMX1921"
 
-export SUPERIOR_OFFICIAL=true
-
 # Maintainer SuperiorOS for RMX1921
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.superior.maintainer=Amrish
